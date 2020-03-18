3ccc41cae848c32117444713bdd1405b5a36b8ed
Pharia Le <pharia.le@gmail.com>
2020-03-18 05:17:25 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,32 @@
 class Transfer
-  # your code here
+  
+  attr_accessor :status
+  attr_reader :sender, :receiver, :amount
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+  
+  def valid?
+    sender.valid? && receiver.valid?  
+  end
+  
+  def execute_transaction
+    if sender.balance < self.amount || !valid?
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    elsif status != "complete"
+      sender.balance -= self.amount
+      receiver.balance += self.amount
+      self.status = "complete"
+    end
+  end
+  
+  def reverse_transfer
+    
+  end
+  
 end
